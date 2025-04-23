e86a927bbed250002ce778bfc9b222bac032ed05
gabrieleCavalcanti <gabrielefurlancavalcanti@yahoo.com>
2025-04-23 11:05:49 -0300

    Inclui resultado no exercício 2

--- exerc_02/index02.js
+++ exerc_02/index02.js
@@ -1,6 +1,7 @@
 let numeroAleatorio = Math.floor((Math.random() * 10) + 1);
 let numeroTentativa = 1;
 let numeroUsuario;
+let resultado = "Derrota!";
 
 do {
     numeroUsuario = parseInt(prompt(`Insira um número de 1 a 10:  `)); 
@@ -11,6 +12,7 @@ do {
 
         if (numeroUsuario == numeroAleatorio) {
             console.log(` Número correto!!`);
+            resultado = "Vitória!!";
         } else if (numeroUsuario > numeroAleatorio) {
             console.log(`O Número é menor`);
         } else {
@@ -21,6 +23,7 @@ do {
 
 } while (numeroUsuario != numeroAleatorio && numeroTentativa <= 3);
 
+console.log(resultado);
 console.log(`O número sortiado era o : ${numeroAleatorio}`);
 
 
